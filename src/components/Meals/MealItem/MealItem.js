97b438e8508ed5ddef202b558fd6b51052ecd07b
cupid chakma <greendaycu20@gmail.com>
2022-10-22 15:42:05 +0600

    added input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MealItemForm from "./MealItemForm";
 import classes from './MealItem.module.css';
 
 
@@ -12,7 +13,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm/>
             </div>
         </li>
     );
